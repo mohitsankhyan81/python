3579b4d4e1d5f613c497cd8d310dc34298a725b9
Mohit <sankhyanm569@gmail.com>
2025-07-01 23:13:50 +0530

    variable type

--- variable.py
+++ variable.py
@@ -1,6 +1,7 @@
 name="mohit";
 rollno=23;
 price=34.12;
+name2=True;
 
 age2=rollno;
 
@@ -9,3 +10,7 @@ print("my name is :- " ,name);
 print("my rollno is :- ",age2);
 print("price is :- ", price);
 
+print(type(name));
+print(type(age2));
+print(type(price));
+print(type(name2))
