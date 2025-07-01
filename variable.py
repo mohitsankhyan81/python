fbc9caf68f1fd74b18f54f3b7e14d14117666d51
Mohit <sankhyanm569@gmail.com>
2025-07-01 23:10:31 +0530

    varable

---
+++ variable.py
@@ -0,0 +1,11 @@
+name="mohit";
+rollno=23;
+price=34.12;
+
+age2=rollno;
+
+print(name,rollno,price);
+print("my name is :- " ,name);
+print("my rollno is :- ",age2);
+print("price is :- ", price);
+
