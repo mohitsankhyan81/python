3c1ec69417f22af2d18b7d444484c32af555a4fc
Mohit <sankhyanm569@gmail.com>
2025-07-02 14:13:08 +0530

    if-else-practice

---
+++ pythonQuestions/if-else-Question.py
@@ -0,0 +1,10 @@
+#input 2 int number
+# print true if a is greater than or equal to b if not print false
+
+num1=int(input("enter num1"));
+num2=int(input("enter num2"));
+
+if(num1>num2):
+    print("True");
+else:
+    print("false");
