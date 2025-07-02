a00be828291be2070041ec8aacf7f71dfbc337f3
Mohit <sankhyanm569@gmail.com>
2025-07-02 14:07:00 +0530

    practice

---
+++ pythonQuestions/input-side-of-a-squere-and-find-its-area.py
@@ -0,0 +1,3 @@
+side=float(input("Enter sides: "));
+
+print("area = ",side*side);
---
+++ pythonQuestions/two-floting-number-and-find-theit-average.py
@@ -0,0 +1,8 @@
+num1=float(input("enter num1 = "));
+num2=float(input("enter num2 = "));
+
+num3=num1+num2;
+
+avg=num3/2;
+
+print("avg is ",avg);
