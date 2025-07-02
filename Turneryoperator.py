27501280f0d3dd7d718b66dab420a9e2d787a949
Mohit <sankhyanm569@gmail.com>
2025-07-02 13:20:00 +0530

    turenry operator

---
+++ Turneryoperator.py
@@ -0,0 +1,6 @@
+# food = input("food :");
+# eat="yes" if food == "cake" else "no";\
+# print(eat);
+
+food=input("food :- ");
+print("sweet") if food == "cake" or food == "jalabi" else print("not sweet");
