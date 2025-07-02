a9a7f46bf8bece02c9fae52e9a43230a926df768
Mohit <sankhyanm569@gmail.com>
2025-07-02 13:49:41 +0530

    operator

---
+++ operator_1.py
@@ -0,0 +1,36 @@
+a = 12
+b = 2
+
+# Some operators
+sum1 = a + b
+subtract = a - b
+multiply = a * b
+divide = a / b
+modulus = a % b
+power =a**b;
+
+print(sum1)
+print(subtract);
+print(multiply)
+print(divide)
+print(modulus)
+print(power);
+
+
+# logical operator
+# not operator
+
+num1=10
+num2=20
+
+print(not False);
+print(not (num1>num2));
+
+# and operator
+val1=True
+val2=False
+
+print("ALL TRUE :" , val1 and val2);
+
+print("OR operator: ",val1 or val2);
+
