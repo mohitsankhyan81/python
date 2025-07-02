b29ac65830c0839f2a885ce2b4f5de8d54466241
Mohit <sankhyanm569@gmail.com>
2025-07-02 06:16:44 +0530

    garade of students

---
+++ pythonQuestions/gradeofstudent-if-else-conditions.py
@@ -0,0 +1,9 @@
+marks=int(input("enter your marks"));
+if(marks>=90):
+    print("A");
+elif(marks>=70 and marks<=85):
+    print("B");
+elif(marks>=40 and marks<=60):
+    print("C");
+else:
+    print("Fail");
