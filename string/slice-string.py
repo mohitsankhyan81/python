57e93a852f4a96ecec85877351c62bd767d7225e
Mohit <sankhyanm569@gmail.com>
2025-07-04 10:08:50 +0530

    COMINT

--- string/slice-string.py
+++ string/slice-string.py
@@ -1,7 +1,11 @@
 # asses part of string
 
-str="mohit sankhyan";
-print(str[1:4]);
+str="mohit sankhyan"
+print(str[1:4])
 
-str1="my name is mohit";
-print(str1[2:len(str1)]);
+str1="my name is mohit"
+print(str1[2:len(str1)])
+
+# slicing negtive indexing
+str2="Mohit Sankhyan"
+print(str2[-12:-1])
