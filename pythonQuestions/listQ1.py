0d485d6d0598c04eef665efafdea96739f55e352
Mohit <mohitsankhyan253@gmail.com>
2025-07-05 09:45:51 +0530

    list Q1

---
+++ pythonQuestions/listQ1.py
@@ -0,0 +1,11 @@
+# "WAP to ask the user to enter names of their 3 favorite movies & store them in a list."
+movies=[]
+mov1=input("Enter 1st movi name")
+mov2=input("Enter 2nd movi name")
+mov3=input("Enter 3rd movi name")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies);
