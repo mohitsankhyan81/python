b853724c147b979e3b2a71ab23ffdd3410de575a
Mohit <mohitsankhyan253@gmail.com>
2025-07-06 09:00:50 +0530

    nested dic

---
+++ dictionary-in-py/nested.dictionary.py
@@ -0,0 +1,13 @@
+student={
+    "name":"Mohit Sankhyan",
+    "subject":{
+        "math":25,
+        "english":30,
+        "gk":100
+    }
+}
+# nested dictionary
+
+print(student)
+print(student["subject"])
+print(student["subject"]["gk"])
