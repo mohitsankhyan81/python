c20fb6c1b6c21ef41be09bcfc562a381257fd296
Mohit <mohitsankhyan253@gmail.com>
2025-07-06 09:29:44 +0530

    set

---
+++ set-in-py/set.py
@@ -0,0 +1,12 @@
+# set is the collection of unordered items
+# each ele,emt in the set is unique ans inmutable
+collection={1,2,3,4,5};
+print(collection)
+print(type(collection))
+print(len(collection))
+# repeated element store once in set
+
+
+# emptyset
+collection1=set()
+print(collection1);
