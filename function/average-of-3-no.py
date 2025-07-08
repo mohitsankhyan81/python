e73251bbe918d20f345b46e436aa62a667f69c82
Mohit <mohitsankhyan253@gmail.com>
2025-07-08 10:02:23 +0530

    len-function

---
+++ function/average-of-3-no.py
@@ -0,0 +1,7 @@
+# average of 3 no
+def sum(a,b,c):
+    sum1=a+b+c
+    ave=sum1/2
+    print(ave)
+
+sum(3,3,3)
---
+++ function/len-count.py
@@ -0,0 +1,7 @@
+list=["mohit","arpit","manish","rohit"]
+
+def fun(list):
+    len=len(list)
+    print(len)
+
+fun()
