fc10f22c8db253d8b02f19a60687db8f576a154a
Mohit <mohitsankhyan253@gmail.com>
2025-07-13 20:56:48 +0530

    constrants

---
+++ oops.py/contructor/cons.py
@@ -0,0 +1,8 @@
+class student:
+    #construct
+    def __init__(self,fullname):
+        self.name=fullname
+        print("adding new student in a new class")
+
+s1=student("rahul")
+print(s1.name)
