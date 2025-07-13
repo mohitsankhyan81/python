f18fb78bf694017dab414cdea335b7f98f70c56a
Mohit <mohitsankhyan253@gmail.com>
2025-07-13 21:22:34 +0530

    presedience

---
+++ oops.py/contructor/class-preferance.py
@@ -0,0 +1,12 @@
+#obj name frefernce os greater than class preference
+
+class student:
+    class_name="ABC collage"
+    name="annonamas"
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+        print("add element in class oops")
+
+s1=student("karan",43)
+print(s1.name,s1.marks)
