52adf48b14e1596e24f2e97c5db4d22c9a4ab4a8
Mohit <mohitsankhyan253@gmail.com>
2025-07-14 19:08:33 +0530

    static method of opps

---
+++ list-in-python/static-method.py
@@ -0,0 +1,9 @@
+#static method is the method that dont use self peremeater 
+
+class student:
+    @staticmethod #this is a decorater in py 
+    def cal():
+        print("my name is mohit\nI am form kalka")
+
+s1=student()
+s1.cal()
