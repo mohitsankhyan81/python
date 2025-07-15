0011d1d43715ca390a0c2dba7c9b3ac29a3868d5
Mohit <mohitsankhyan253@gmail.com>
2025-07-15 11:20:00 +0530

    changes

--- list-in-python/Q2.py
+++ list-in-python/Q2.py
@@ -1,21 +1,4 @@
-class account:
-    def __init__(self, bal, acc):
-        self.balance = bal
-        self.account = acc
 
-    def devit(self, ammount):
-        self.balance -= ammount
-        print("Rs", ammount, "are devited")
-
-    def credit(self, ammount):
-        self.balance += ammount
-        print("Rs", ammount, "are credited")
-
-    def getbalance(self, ammount):
-        return self.balance
-
-person1 = account(10000, 12345)
-person1.credit(500)
 class account:
     def __init__(self, bal, acc):
         self.balance = bal
